f63b06d396c9525ad4e46ec1afc393f05334d9d0
Fabiano Amorim <fabianonevesamorim@hotmail.com>
2024-06-19 10:39:19 -0300

    fixing issue with partitioned indexes

--- IndexChecks/Check45-Compression savings.sql
+++ IndexChecks/Check45-Compression savings.sql
@@ -1506,6 +1506,7 @@ BEGIN
          [table_option_ddl],
          [sample_percent]
   FROM (SELECT DISTINCT
+               [index_id],
                [partition_column_id],
                [partition_function_id],
                [partition_number],
@@ -1518,7 +1519,7 @@ BEGIN
                       END AS [sample_percent]
                FROM [sys].[dm_db_partition_stats] [ps]
                WHERE [ps].[object_id] = @object_id
-                     AND [index_id] < 2
+                     AND [ps].[index_id] = [t].[index_id]
                      AND [ps].[partition_number] = [t].[partition_number]) [ps]
   CROSS APPLY [dbo].index_review_generate_table_sample_ddl(
                 @object_id,
