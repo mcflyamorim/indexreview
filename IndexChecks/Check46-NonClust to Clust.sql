e49e34a338f391787cc37d13caeec1745d1f7df0
Fabiano Amorim <fabianonevesamorim@hotmail.com>
2025-01-07 04:51:04 -0300

    .

--- IndexChecks/Check46-NonClust to Clust.sql
+++ IndexChecks/Check46-NonClust to Clust.sql
@@ -184,7 +184,7 @@ CROSS APPLY (SELECT CASE
                         CONVERT(
                           VARCHAR,
                           CONVERT(
-                            NUMERIC(18, 2), ((ISNULL(b.[user_lookups], 0) * 1.00) / ISNULL(a.[user_seeks], 0)) * 100.0))
+                            NUMERIC(18, 2), ((ISNULL(a.[user_seeks], 0) * 1.00) / ISNULL(b.[user_lookups], 0)) * 100.0))
                         + '%'
                     END AS ratio_of_index_seeks_vs_base_table_lookups) AS Tab2
 WHERE 1 = 1
